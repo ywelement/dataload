0fe7161a0921a684269c20f2b2b1aefd0b49e327
Yang Wang <yw@discoverelement.com>
2016-08-22 17:37:18 -0400

    set sample properties

--- loadImageNetPatchSet.lua
+++ loadImageNetPatchSet.lua
@@ -1,3 +1,11 @@
+--[[
+  Script to load image patches from ImageNet
+  It requires threads and torchx packages, 
+  which can be installed by the following commands:
+    luarocks install threads
+    luarocks install torchx
+--]]
+
 local dl = require 'dataload._env'
 
 -- load ILSVRC2012-14 image classification dataset (A.K.A. ImageNet)
@@ -8,7 +16,8 @@ local dl = require 'dataload._env'
 -- Due to its size, the data first needs to be prepared offline :
 -- 1. use scripts/downloadimagenet.lua to download and extract the data
 -- 2. use scripts/harmonizeimagenet.lua to harmonize train/valid sets
-function dl.loadImageNetPatchSet(datapath, nthread, samplesize, samplenorm, sampleperimage, 
+function dl.loadImageNetPatchSet(datapath, nthread, samplesize, samplenorm, 
+                                 trainsampleperimage, testsampleperimage,
                                  traincenterfirst, testcenterfirst, verbose)
    -- 1. arguments and defaults
    
@@ -21,7 +30,8 @@ function dl.loadImageNetPatchSet(datapath, nthread, samplesize, samplenorm, samp
    -- normalize samples
    samplenorm = samplenorm or false
    -- how many samples per image
-   sampleperimage = sampleimage or 1
+   trainsampleperimage = trainsampleperimage or 1
+   testsampleperimage = testsampleperimage or 1
    -- crop the center patch first
    traincenterfirst = traincenterfirst or false
    testcenterfirst = testcenterfirst or false
@@ -30,8 +40,14 @@ function dl.loadImageNetPatchSet(datapath, nthread, samplesize, samplenorm, samp
 
    -- path to training images
    local trainpath = paths.concat(datapath, 'ILSVRC2012_img_train')
+   if not paths.dirp(trainpath) then
+      trainpath = paths.concat(datapath, 'Train')
+   end
    -- path to validation images
    local validpath = paths.concat(datapath, 'ILSVRC2012_img_val')
+   if not paths.dirp(validpath) then
+      validpath = paths.concat(datapath, 'Test')
+   end
    -- path to meta data
    local metapath = paths.concat(datapath, 'metadata')
    
@@ -42,8 +58,15 @@ function dl.loadImageNetPatchSet(datapath, nthread, samplesize, samplenorm, samp
    local train = dl.ImagePatchSet(trainpath, samplesize, 'sampleTrain', sortfunc, verbose)
    local valid = dl.ImagePatchSet(validpath, samplesize, 'sampleTest',  sortfunc, verbose)
 
-   setSampleProperties(samplenorm, sampleperimage, traincenterfirst, testcenterfirst)
+   if verbose then
+      print('setSampleProperties', samplenorm, sampleperimage, traincenterfirst, testcenterfirst)
+   end
+   train:setSampleProperties(samplenorm, sampleperimage, traincenterfirst, testcenterfirst)
+   valid:setSampleProperties(samplenorm, sampleperimage, traincenterfirst, testcenterfirst)
    
+   if verbose then
+      print('set AsyncIterator on '..nthread..' threads')
+   end
    train = dl.AsyncIterator(train, nthread)
    valid = dl.AsyncIterator(valid, nthread)
    
